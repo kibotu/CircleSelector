aedbc6ab5509764500c0e174debf8a91d77942f8
Jan Rabe <jan.rabe@exozet.com>
2017-03-14 18:52:02 +0100

    fixed compile error and updated gradle wrapper

--- app/src/main/java/net/kibotu/circleselector/app/MainActivity.java
+++ app/src/main/java/net/kibotu/circleselector/app/MainActivity.java
@@ -2,12 +2,7 @@ package net.kibotu.circleselector.app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import net.kibotu.urlshortener.UrlShortener;
-
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,21 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        UrlShortener.setEnableLogging(true);
-
-        UrlShortener.shortenUrlByTinyUrl("http://www.google.com")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(r -> {
-                    Log.v(TAG, "[onCreate] shortenUrlByTinyUrl " + r);
-                }, Throwable::printStackTrace);
-
-        UrlShortener.shortenUrlByGoogle(this, "http://www.google.com")
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(r -> {
-                    Log.v(TAG, "[onCreate] shortenUrlByGoogle " + r);
-                }, Throwable::printStackTrace);
     }
 }
